ac6f40a53f942045af583e05d3b413db8164135e
Iryna Volosozharova <iradevis@gmail.com>
2021-08-01 19:36:16 +0300

    finished task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Component } from "react";
-// import { useState, useEffect, useRef } from "react";
+// import { Component, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Searchbar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery";
 import Button from "./components/Button";
@@ -11,16 +11,131 @@ import fetchImages from "./Services/ImagesAPI";
 import "modern-normalize";
 import "./styles.css";
 
-// export default function App() {
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [images, setImages] = useState({});
-//   const [modalImage, setModalImage] = useState('');
-//   const [page, setPage] = useState(1);
-//   const [loader, setLoader] = useState(false);
-//   const [error, setError] = useState('');
-//   const isFirstRender = useRef(true);
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [images, setImages] = useState([]);
+  const [modalImage, setModalImage] = useState("");
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState("");
+  const renderLoadButton = images.length >= 12 && !loader;
 
-//   return (
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    setLoader(true);
+    fetchImages(searchQuery, page)
+      .then((images) => {
+        setImages((prev) => [...prev, ...images]);
+      })
+      .catch(setError)
+      .finally(setLoader(false));
+  }, [searchQuery, page]);
+
+  const changeSearch = (query) => {
+    if (query === searchQuery) {
+      return;
+    }
+
+    setSearchQuery(query);
+    setImages([]);
+    setPage(1);
+    setError("");
+  };
+
+  const openModalImage = (img) => {
+    setModalImage(img);
+    setShowModal(!showModal);
+  };
+
+  return (
+    <>
+      <Searchbar onSubmit={changeSearch} />
+
+      <ImageGallery images={images} onClick={openModalImage} />
+
+      {loader && <Loader />}
+
+      {error && <p>Sorry! Somethimg went wrong... Try again, please!</p>}
+
+      {renderLoadButton && (
+        <Button
+          onClick={() => setPage((prev) => prev + 1)}
+          length={images.length}
+        />
+      )}
+
+      {showModal && (
+        <Modal onClose={() => setShowModal(!showModal)} image={modalImage} />
+      )}
+    </>
+  );
+}
+
+// class App extends Component {
+//   state = {
+//     searchQuery: "",
+//     images: [],
+//     modalImage: "",
+//     page: 1,
+//     loader: false,
+//     showModal: false,
+//     error: "",
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       this.getImages();
+//     }
+//   }
+
+// changeSearch = (query) => {
+//   if (query === this.state.searchQuery) {
+//     return;
+//   }
+//   this.setState({
+//     searchQuery: query,
+//     images: [],
+//     page: 1,
+//     error: "",
+//   });
+// };
+
+//   getImages = () => {
+//     const { page, searchQuery } = this.state;
+
+//     this.setState({ loader: true });
+
+// fetchImages(searchQuery, page)
+//   .then((images) => {
+//     this.setState((prevState) => ({
+//       images: [...prevState.images, ...images],
+//       page: prevState.page + 1,
+//     }));
+//   })
+//   .catch((error) => this.setState({ error }))
+//   .finally(() => this.setState({ loader: false }));
+//   };
+
+//   toggleModal = () => {
+//     this.setState({ showModal: !this.state.showModal });
+//   };
+
+// openModalImage = (img) => {
+//   this.setState({
+//     modalImage: img,
+//   });
+//   this.toggleModal();
+// };
+
+//   render() {
+//     const { images, loader, showModal, modalImage } = this.state;
+//     const { changeSearch, openModalImage, getImages, toggleModal } = this;
+// const renderLoadButton = images.length >= 12 && !loader;
+
+//     return (
 //       <>
 //         <Searchbar onSubmit={changeSearch} />
 
@@ -35,85 +150,7 @@ import "./styles.css";
 //         {showModal && <Modal onClose={toggleModal} image={modalImage} />}
 //       </>
 //     );
-//  }
+//   }
+// }
 
-class App extends Component {
-  state = {
-    searchQuery: "",
-    images: [],
-    modalImage: "",
-    page: 1,
-    loader: false,
-    showModal: false,
-    error: "",
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.getImages();
-    }
-  }
-
-  changeSearch = (query) => {
-    if (query === this.state.searchQuery) {
-      return;
-    }
-    this.setState({
-      searchQuery: query,
-      images: [],
-      page: 1,
-      error: "",
-    });
-  };
-
-  getImages = () => {
-    const { page, searchQuery } = this.state;
-
-    this.setState({ loader: true });
-
-    fetchImages(searchQuery, page)
-      .then((images) => {
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...images],
-          page: prevState.page + 1,
-        }));
-      })
-      .catch((error) => this.setState({ error }))
-      .finally(() => this.setState({ loader: false }));
-  };
-
-  toggleModal = () => {
-    this.setState({ showModal: !this.state.showModal });
-  };
-
-  openModalImage = (img) => {
-    this.setState({
-      modalImage: img,
-    });
-    this.toggleModal();
-  };
-
-  render() {
-    const { images, loader, showModal, modalImage } = this.state;
-    const { changeSearch, openModalImage, getImages, toggleModal } = this;
-    const renderLoadButton = images.length >= 12 && !loader;
-
-    return (
-      <>
-        <Searchbar onSubmit={changeSearch} />
-
-        <ImageGallery images={images} onClick={openModalImage} />
-
-        {loader && <Loader />}
-
-        {renderLoadButton && (
-          <Button onClick={getImages} length={images.length} />
-        )}
-
-        {showModal && <Modal onClose={toggleModal} image={modalImage} />}
-      </>
-    );
-  }
-}
-
-export default App;
+// export default App;
